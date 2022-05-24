147b0e60383df712a6375b5840239cf3137e9923
faustaciku <faustaciku@gmail.com>
2022-05-24 13:20:12 +0100

    Improved date format

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./ExpenseItem.css";
 export default function ExpenseItem(props) {
+  const month = props.date.toLocaleString(`en-US`, { month: `long` });
+  const day = props.date.toLocaleString(`en-US`, { day: `2-digit` });
+  const year = props.date.getFullYear();
   return (
     <div className="ExpenseItem">
-      <div className="item-description">{props.date.toISOString()}</div>
-      <h2>{props.title}</h2>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="item-description">
+        <h2>{props.title}</h2>
+      </div>
       <div className="item-price">$ {props.amount}</div>
     </div>
   );
