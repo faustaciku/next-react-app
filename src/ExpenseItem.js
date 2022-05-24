4788f16330a8a359336eb1cce485e39d6a970fbe
faustaciku <faustaciku@gmail.com>
2022-05-24 13:31:41 +0100

    Moved date in a new component and added styling for it

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,16 +1,10 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 export default function ExpenseItem(props) {
-  const month = props.date.toLocaleString(`en-US`, { month: `long` });
-  const day = props.date.toLocaleString(`en-US`, { day: `2-digit` });
-  const year = props.date.getFullYear();
   return (
     <div className="ExpenseItem">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="item-description">
         <h2>{props.title}</h2>
       </div>
