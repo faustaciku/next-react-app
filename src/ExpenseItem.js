1b3a62d7b011d3de74104579d0f9fe036acf3adc
faustaciku <faustaciku@gmail.com>
2022-05-24 13:07:34 +0100

    Duplicated a component with dummy data

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./ExpenseItem.css";
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
     <div className="ExpenseItem">
-      <div className="item-description">May 24th 2022</div>
-      <h2>Car insurance</h2>
-      <div className="item-price">$294.67</div>
+      <div className="item-description">{props.date.toISOString()}</div>
+      <h2>{props.title}</h2>
+      <div className="item-price">$ {props.amount}</div>
     </div>
   );
 }
