f27ba5966adfdb188a754ace62956eb3ebbf8808
faustaciku <faustaciku@gmail.com>
2022-05-25 13:50:55 +0100

    Extracted duplicating style in a Card component

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./ExpenseItem.css";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
+
 export default function ExpenseItem(props) {
   return (
-    <div className="ExpenseItem">
+    <Card className="ExpenseItem">
       <ExpenseDate date={props.date} />
       <div className="item-description">
         <h2>{props.title}</h2>
       </div>
       <div className="item-price">$ {props.amount}</div>
-    </div>
+    </Card>
   );
 }
