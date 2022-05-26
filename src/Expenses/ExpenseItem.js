79eb36c7d1fb9633ab8f183389ca799caecf84f4
faustaciku <faustaciku@gmail.com>
2022-05-26 11:13:39 +0100

    Deleted unnecessary button

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import Card from "../Card";
 import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  function clickHandler() {
-    setTitle("Updated");
-  }
   return (
     <Card className="ExpenseItem">
       <ExpenseDate date={props.date} />
       <div className="item-description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <button onClick={clickHandler}>Change title</button>
       <div className="item-price">$ {props.amount}</div>
     </Card>
   );
